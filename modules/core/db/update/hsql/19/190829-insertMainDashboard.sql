48c63f7cb186f6a7e5e9a13f2f05c04fdf898176
Mikhail Dyakonov <dyakonov.mikhail@gmail.com>
2019-08-29 18:05:55 +0400

    adding dashboard to main window

---
+++ modules/core/db/update/hsql/19/190829-insertMainDashboard.sql
@@ -0,0 +1,148 @@
+INSERT INTO DASHBOARD_PERSISTENT_DASHBOARD
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, DASHBOARD_MODEL, NAME, REFERENCE_NAME, GROUP_ID, IS_AVAILABLE_FOR_ALL_USERS)
+VALUES('83c3b560-bec2-1777-284d-d8a65acaaade', 1, '2019-08-29 17:58:18.394', 'admin', '2019-08-29 17:58:18.394', NULL, NULL, NULL, '{
+  "title": "Main Dashboard",
+  "code": "main-dashboard",
+  "visualModel": {
+    "children": [
+      {
+        "className": "com.haulmont.addon.dashboard.model.visualmodel.ResponsiveLayout",
+        "data": {
+          "xs": 12,
+          "sm": 6,
+          "md": 6,
+          "lg": 3,
+          "areas": [
+            {
+              "component": {
+                "className": "com.haulmont.addon.dashboard.model.visualmodel.WidgetLayout",
+                "data": {
+                  "widget": {
+                    "showWidgetCaption": false,
+                    "widgetId": "random-joke2",
+                    "caption": "Random Joke",
+                    "name": "Random Joke",
+                    "description": null,
+                    "parameters": [],
+                    "widgetFields": [],
+                    "frameId": "petclinic_RandomJokeWidget",
+                    "createdBy": "admin",
+                    "id": "0644d886-271f-41d4-ab2d-6d7794b0659b",
+                    "__state": 0,
+                    "__securityState": null,
+                    "dynamicAttributes": null,
+                    "_persistence_fetchGroup": null
+                  },
+                  "children": [],
+                  "weight": 1,
+                  "expand": null,
+                  "styleName": null,
+                  "width": 100,
+                  "height": 100,
+                  "widthUnit": "%",
+                  "heightUnit": "%",
+                  "id": "7db23ddf-f3d2-7ce0-a8cb-7bb6c42cc6bc",
+                  "__state": 1,
+                  "__securityState": null,
+                  "dynamicAttributes": null,
+                  "_persistence_fetchGroup": null
+                }
+              },
+              "order": 1,
+              "xs": 12,
+              "sm": 6,
+              "md": 6,
+              "lg": 6,
+              "id": "471c7211-0cb8-eac4-4f0a-91dcb21532a0",
+              "__state": 1,
+              "__securityState": null,
+              "dynamicAttributes": null,
+              "_persistence_fetchGroup": null
+            },
+            {
+              "component": {
+                "className": "com.haulmont.addon.dashboard.model.visualmodel.WidgetLayout",
+                "data": {
+                  "widget": {
+                    "showWidgetCaption": false,
+                    "widgetId": "visits-calendar2",
+                    "caption": "Visits Calendar",
+                    "name": "Visits Calendar",
+                    "description": null,
+                    "parameters": [],
+                    "widgetFields": [],
+                    "frameId": "petclinic_VisitsCalendarWidget",
+                    "createdBy": "admin",
+                    "id": "9d5f9f98-e2fa-b94c-feb2-7c24ca642d7c",
+                    "__state": 0,
+                    "__securityState": null,
+                    "dynamicAttributes": null,
+                    "_persistence_fetchGroup": null
+                  },
+                  "children": [],
+                  "weight": 1,
+                  "expand": null,
+                  "styleName": null,
+                  "width": 100,
+                  "height": 100,
+                  "widthUnit": "%",
+                  "heightUnit": "%",
+                  "id": "ed8d0d8c-5322-8984-a977-97a9c6f86668",
+                  "__state": 1,
+                  "__securityState": null,
+                  "dynamicAttributes": null,
+                  "_persistence_fetchGroup": null
+                }
+              },
+              "order": 2,
+              "xs": 12,
+              "sm": 6,
+              "md": 6,
+              "lg": 6,
+              "id": "4a4ca612-56ee-9ea1-1893-1e50a4974d58",
+              "__state": 1,
+              "__securityState": null,
+              "dynamicAttributes": null,
+              "_persistence_fetchGroup": null
+            }
+          ],
+          "children": [],
+          "weight": 1,
+          "expand": null,
+          "styleName": null,
+          "width": 100,
+          "height": 100,
+          "widthUnit": "%",
+          "heightUnit": "%",
+          "id": "7a8e258e-ebe3-a572-55b6-b86d028df932",
+          "__state": 1,
+          "__securityState": null,
+          "dynamicAttributes": null,
+          "_persistence_fetchGroup": null
+        }
+      }
+    ],
+    "weight": 1,
+    "expand": null,
+    "styleName": null,
+    "width": 100,
+    "height": 100,
+    "widthUnit": "%",
+    "heightUnit": "%",
+    "id": "ef46568f-3e63-827a-b028-87144ce1147a",
+    "__state": 1,
+    "__securityState": null,
+    "dynamicAttributes": null,
+    "_persistence_fetchGroup": null
+  },
+  "parameters": [],
+  "isAvailableForAllUsers": true,
+  "createdBy": "admin",
+  "timerDelay": 0,
+  "assistantBeanName": null,
+  "id": "3bac87aa-7ce1-4b51-1828-faa724272d3d",
+  "__state": 1,
+  "__securityState": null,
+  "dynamicAttributes": null,
+  "_persistence_fetchGroup": null
+}', 'Main Dashboard', 'main-dashboard', NULL, true);
